c7f28dca140b36226927a07007fa0f822f70a539
PalmNeko <nitunamoni13579@gmail.com>
2025-05-10 06:17:05 +0900

    add: nginx

--- submit/srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ submit/srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
 # docker-entrypoint.sh
-echo "run nginx!!!"
 
-if [ "$1" = "nginx" ]; then
-    nginx -s stop
-fi
+main() {
+    echo "[ into docker-entry.sh $@ ]"
 
-echo "$@"
+    if [ "$1" = "nginx" ]; then
+        service --status-all 2> /dev/stdout | grep nginx | grep '+' \
+            && nginx -s stop && echo "stop nginx!!" \
+                # if nginx is beginning, stop nginx
+        chk_nginx
+    fi
 
-exec "$@"
+    echo "[ out of docker-entry.sh ]"
+
+    echo "run nginx!!!"
+    exec "$@"
+}
+
+chk_nginx() {
+    # import environment from .env
+    # - DOMAIN
+    DOMAIN=$DOMAIN
+    SSL_KEYFILE="/ssl/nginx.key"
+    SSL_CRTFILE="/ssl/nginx.crt"
+
+    test -f "$SSL_KEYFILE" || errexit "$SSL_KEYFILE: No such file or directory - check Dockerfile in prepare stage"
+    echo "$SSL_KEYFILE: exists"
+    test -f "$SSL_CRTFILE" || errexit "$SSL_KEYFILE: No such file or directory - check Dockerfile in prepare stage"
+    echo "$SSL_CRTFILE: exists"
+}
+
+errexit() {
+    echo "$@" > /dev/stderr
+    exit 1
+}
+
+main "$@"
