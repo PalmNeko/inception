7025b4ddd5d71464be5615d858e02abbb51706ef
PalmNeko <nitunamoni13579@gmail.com>
2025-05-10 06:28:48 +0900

    update: nginx

--- submit/srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ submit/srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -9,6 +9,7 @@ main() {
         service --status-all 2> /dev/stdout | grep nginx | grep '+' \
             && nginx -s stop && echo "stop nginx!!" \
                 # if nginx is beginning, stop nginx
+        make_cert
         chk_nginx
     fi
 
@@ -16,6 +17,24 @@ main() {
 
     echo "run nginx!!!"
     exec "$@"
+}
+
+make_cert() {
+    # import environment from .env
+    # - DOMAIN
+    DOMAIN=$DOMAIN
+    SSL_KEYFILE="/ssl/nginx.key"
+    SSL_CRTFILE="/ssl/nginx.crt"
+
+    mkdir -p $(dirname "$SSL_KEYFILE")
+    mkdir -p $(dirname "$SSL_CRTFILE")
+
+    openssl req -x509 -nodes -days 15 -newkey rsa:2048 \
+    -keyout "$SSL_KEYFILE" -out "$SSL_CRTFILE" \
+    -subj "/CN=${DOMAIN}"
+
+    test -f "$SSL_KEYFILE" || errexit "$SSL_KEYFILE: Creation failure."
+    test -f "$SSL_CRTFILE" || errexit "$SSL_KEYFILE: Creation failure."
 }
 
 chk_nginx() {
