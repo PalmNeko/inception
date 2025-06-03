9dfe4bdb49b57ae4a5bac4652bf2b431f46ba9ce
PalmNeko <nitunamoni13579@gmail.com>
2025-06-03 23:42:14 +0900

    add: wordpress user to mariadb

--- submit/srcs/requirements/mariadb/conf/wp.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER wordpress@localhost IDENTIFIED BY 'password';
-CREATE DATABASE wordpress;
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER
-ON wordpress.*
-TO wordpress@localhost
-IDENTIFIED BY 'password';
-FLUSH PRIVILEGES;
