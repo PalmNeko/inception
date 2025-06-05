b1678300364f59c9cbd3017d66189310e2f1d0bc
PalmNeko <nitunamoni13579@gmail.com>
2025-06-05 20:59:05 +0900

    add: wordpress data

--- submit/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ submit/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -2,9 +2,10 @@
 
 main () {
 	echo '[ start wordpress container ]'
-
+	env
 	if [ "$1" = "php-fpm7.4" ]; then
 		setup_wordpress
+		for_debug_php_info
 	fi
 	echo '[ the setup is now complete ]'
 	exec "$@"
@@ -13,7 +14,20 @@ main () {
 setup_wordpress () {
 	echo '[ into setup_wordpress ]'
 	mkdir -p /run/php
+	extract_wordpress
 	echo '[ out of setup_wordpress ]'
+}
+
+extract_wordpress() {
+    mkdir -p /srv/wordpress
+	if find /srv/wordpress -type f,d | grep "" > /dev/null; then
+		echo 'extract wordpress'
+    	tar -xf /wordpress.tar.gz -C /srv
+		chown -R root:www-data /srv
+		echo 'extracted'
+	fi
+	chmod 0775 /srv/wordpress
+	chown root:www-data /srv/wordpress
 }
 
 main "$@"
