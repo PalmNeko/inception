aa7b6fda345b6be58f2c595784f8a9336457af04
PalmNeko <nitunamoni13579@gmail.com>
2025-06-05 15:37:25 +0900

    add: init_file script

--- submit/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ submit/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -5,6 +5,10 @@ main () {
 
 	if [ "$1" = "mariadbd" ]; then
 		setup_mariadbd
+		if [ -n "$INIT_DB_FILE" ]; then
+			merge_init_file
+			exec "$@" --init-file="$INIT_DB_FILE"
+		fi
 	fi
 	echo '[ the setup is now complete ]'
 	exec "$@"
@@ -31,6 +35,20 @@ setup_directory__pid_file() {
 	mkdir -p "$PID_FILE_DIRECTORY"
 	chown -R mysql:mysql "$PID_FILE_DIRECTORY"
 	ls -ld "$PID_FILE_DIRECTORY"
+}
+
+merge_init_file() {
+	INIT_DB_FILE_DIR=/etc/mysql/initdb.d
+	INIT_DB_FILE=/etc/mysql/initdb.sql
+	local init_db_file_dir="${INIT_DB_FILE_DIR:-/etc/mysql/initdb.d}"
+	local init_db_file="${INIT_DB_FILE:-/etc/mysql/initdb.sql}"
+	echo 'merge init-files'
+	mkdir -p "$init_db_file_dir"
+	touch "$init_db_file"
+	for file in $(find "$init_db_file_dir" -name "*.sql" | sort); do
+		cat "$file" >> "$init_db_file"
+		echo -e ';' >> "$init_db_file"
+	done
 }
 
 main "$@"
